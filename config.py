ac43763e15c7625039956ab2bfb794a9b0f91e84
hecker13-coder <kuldeepyogi1210@gmail.com>
2025-04-12 03:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "23249365"))
 API_HASH = os.environ.get("API_HASH", "2791f2caf27628d7fc3588b8217fb780")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "7531146102"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002602056543"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1274018986"))
@@ -66,7 +66,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(7531146102)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
